a1fa3bf48f2a41f38133d594ff3ac2f2b95a32a2
Jax Taylor <jax.maximillion.taylor@gmail.com>
2019-06-26 19:51:28 -0400

    doing some badass api calls

---
+++ app.js
@@ -0,0 +1,56 @@
+const https = require('https');
+const http = require('http');
+const url = require('url');
+const apiKey = '0af18737722b3d867cbf25114c09f760';
+const location = process.argv.slice(2);
+
+function getWeather(cityId) {
+    const request = https.get(`https://api.openweathermap.org/data/2.5/weather?id=${cityId}&APPID=${apiKey}`, (response) => {
+        let body = '';
+        response.on('data', (data) => {
+            body += data.toString();
+        });
+
+        response.on('end', () => {
+            const weatherInfo = JSON.parse(body);
+            console.dir(weatherInfo);
+            console.log(`It is currently ${weatherInfo.main.temp/10} degrees celsius in ${weatherInfo.name}`)
+        });
+    })
+}
+
+console.log(location)
+getWeather(4188377);
+
+
+const sampleWeather = { 
+    coord: { lon: -80.11, lat: 26.5 },
+    weather:
+    [ { id: 802,
+        main: 'Clouds',
+        description: 'scattered clouds',
+        icon: '03d' } ],
+    base: 'stations',
+    main:
+    { temp: 303.91,
+        pressure: 1017,
+        humidity: 62,
+        temp_min: 301.48,
+        temp_max: 305.37 },
+    visibility: 16093,
+    wind: { speed: 6.2, deg: 70, gust: 7.7 },
+    clouds: { all: 40 },
+    dt: 1561591436,
+    sys:
+    { type: 1,
+        id: 5314,
+        message: 0.0076,
+        country: 'US',
+        sunrise: 1561544956,
+        sunset: 1561594623 },
+    timezone: -14400,
+    id: 4148411,
+    name: 'Boca Raton',
+    cod: 200 
+};
+
