ef8b7c75ff9a5464b2d54c93df6c2e7998592c48
Jax Taylor <jax.maximillion.taylor@gmail.com>
2019-06-27 09:17:53 -0400

    thought i looked cute, might delete later

--- .history/app_20190626214213.js
+++ .history/app_20190626214213.js
@@ -1,4 +1,2 @@
 const weather = require('./weather');
 // join multiple values passed as arguments and replace all spaces with underscores
-const query = process.argv.slice(2).join("_").replace(' ', '_');
-weather.get(query);
