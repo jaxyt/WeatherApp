b107fd4fdc7ed0f44f04eaf22a4210ba74dfa0c9
narrow2 <narrow2@AllPurpJaxsiMac.network.narrowpath.biz>
2019-06-27 11:32:33 -0400

    the size of the citylist json file was messing with my code editor

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-const weather = require('./weather');
-// join multiple values passed as arguments and replace all spaces with underscores
-const query = process.argv.slice(2).join("_").replace(' ', '_');
-weather.get(query);
+const https = require('https');
+const apiKey = '0af18737722b3d867cbf25114c09f760';
+const location = process.argv.slice(2);
+
+
+
+function weather(args) {
+    try {
+        const method = args[0];
+        const main = args[1];
+        const request = https.get(`https://api.openweathermap.org/data/2.5/weather?${method}=${main}&APPID=${apiKey}`, (res) => {
+            let body = ""; 
+            res.on('data', (chunk) => {
+                body += chunk.toString();
+            });
+            res.on('end', () => {
+                const weatherData = JSON.parse(body);
+                console.dir(weatherData);
+            });
+        }); 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+weather(location);
