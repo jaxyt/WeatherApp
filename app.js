3c6933fe697aff8dc128a4c6f181f0e13c93ccb3
Jax Taylor <jax.maximillion.taylor@gmail.com>
2019-06-27 12:16:34 -0400

    added try-catch blocks for error handling

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 const https = require('https');
+const http = require('http');
 const apiKey = '0af18737722b3d867cbf25114c09f760';
 const location = process.argv.slice(2);
 
-
+function printError(error) {
+    console.error(error)
+}
 
 function weather(args) {
     try {
         const method = args[0];
         const main = args[1];
         const request = https.get(`https://api.openweathermap.org/data/2.5/weather?${method}=${main}&APPID=${apiKey}`, (res) => {
-            let body = ""; 
-            res.on('data', (chunk) => {
-                body += chunk.toString();
-            });
-            res.on('end', () => {
-                const weatherData = JSON.parse(body);
-                console.log(`it is currently ${(weatherData.main.temp * (9/5) - 459.67).toFixed(1)}F in ${weatherData.name}`);
-            });
+            if (res.statusCode === 200) {
+                let body = ""; 
+                res.on('data', (chunk) => {
+                    body += chunk.toString();
+                });
+                res.on('end', () => {
+                    try {
+                        const weatherData = JSON.parse(body);
+                        console.log(`it is currently ${(weatherData.main.temp * (9/5) - 459.67).toFixed(1)}F in ${weatherData.name}`);
+                    } catch (error) {
+                        printError(error);
+                    }
+                });
+            } else {
+                const statusCodeError = new Error(`${http.STATUS_CODES[res.statusCode]}`);
+                printError(statusCodeError);
+            }
+            
         }); 
     } catch (error) {
-        console.error(error);
+        printError(error);
     }
 }
 
